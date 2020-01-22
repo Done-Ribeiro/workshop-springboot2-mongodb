e5fd123888322f648af52aa664bd24cca2a5314c
Done Ribeiro <www.done_@hotmail.com>
2020-01-22 09:38:36 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/done/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.done.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.done.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
