fe2d11d3345675d2c1ade8cfc7e8319623fe5f11
Done Ribeiro <www.done_@hotmail.com>
2020-01-22 15:03:42 -0300

    Consulta simples com @query

--- src/main/java/com/done/workshopmongo/services/PostService.java
+++ src/main/java/com/done/workshopmongo/services/PostService.java
@@ -22,6 +22,6 @@ public class PostService {
 	}
 
 	public List<Post> findByTitle(String text) {
-		return postReposiroty.findByTitleContainingIgnoreCase(text);
+		return postReposiroty.searchTitle(text);
 	}
 }
