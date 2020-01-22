37fb8c8c8748686ccfd0fd6e15b6820eb81a2916
Done Ribeiro <www.done_@hotmail.com>
2020-01-22 11:56:55 -0300

    Obtendo um post por id

---
+++ src/main/java/com/done/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.done.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.done.workshopmongo.domain.Post;
+import com.done.workshopmongo.repository.PostRepository;
+import com.done.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postReposiroty;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postReposiroty.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
