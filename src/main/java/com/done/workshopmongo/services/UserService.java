b38161afd369e7940414763a17bad3617ebae627
Done Ribeiro <www.done_@hotmail.com>
2020-01-21 21:12:54 -0300

    Insercao de usuario com POST

--- src/main/java/com/done/workshopmongo/services/UserService.java
+++ src/main/java/com/done/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.done.workshopmongo.domain.User;
+import com.done.workshopmongo.dto.UserDTO;
 import com.done.workshopmongo.repository.UserRepository;
 import com.done.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userReposiroty.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return userReposiroty.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
